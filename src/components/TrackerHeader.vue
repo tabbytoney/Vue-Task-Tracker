698e5a93febdb29172de1ddcc7b4e68fe66858b5
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-02-27 14:16:49 -0600

    Button component. Passing props

---
+++ src/components/TrackerHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name:'TrackerHeader',
+        //  You can define props as an array or an object
+        // props: ['title']
+      
+        // To add a default value:
+        // props: {
+        //     title: {
+        //         type: String,
+        //         default: 'Hello World',
+        //     }
+        // }
+
+          props: {
+            title: String
+        }
+    }
+</script>
+
+// Scoped = these styles are only for this component
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+</style>
