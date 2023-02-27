31e4a03d695f2af8689f301eea03f3640a521d26
Tabby Toney Douglass <tabbytoney@gmail.com>
2023-02-27 16:35:41 -0600

    Tasks component, individual task component, icons

---
+++ src/components/Task.vue
@@ -0,0 +1,40 @@
+<template>
+  <!--  ternary if it has a reminder or not. But always want the task class. -->
+  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.text }}
+      <i class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scope>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- take in a prop of the tasks (as an array) then loop through them
+     then output a single task component for each one -->
+  <div :key="task.id" v-for="task in tasks">
+    <!-- For each individual task: -->
+    <!-- <h3>{{ task.test }}</h3> -->
+    <!-- The below 'task' renders the individual object -->
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+// take in tasks array as a prop
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+};
+</script>
